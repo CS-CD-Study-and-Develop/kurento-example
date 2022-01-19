b87aefaa3f0de05595d844224e043293f3e1db2e
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-19 16:28:55 +0900

    socket connection between back and front

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
-import path from 'path';
+import http from "http";
+import path from "path";
+import WebSocket, { WebSocketServer } from 'ws';
 const __dirname = path.resolve();
 
 const app = express();
@@ -10,6 +12,21 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+function handleConnection(socket) {
+    // console.log(socket);
+    console.log("connected");
+}
+
+wss.on("connection", (socket) => {
+    console.log("connected");
+});
+
+server.listen(3000, handleListen);
