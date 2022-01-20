d07d50ec547c6ea28e34d6ecf8011511a4f8a552
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-20 13:52:33 +0900

    join room and send message to all

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,8 +9,18 @@ let roomName;
 function addMessage(message) {
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
-    li.innerText = "Someone joined";
+    li.innerText = message;
     ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", value, roomName, () => {
+        addMessage(`You: ${value}`)
+    });
+    input.value = "";
 }
 
 function showRoom() {
@@ -18,6 +28,8 @@ function showRoom() {
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room: ${roomName}`;
+    const form = room.querySelector("form");
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -33,3 +45,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
     addMessage("someone joined");
 })
+
+socket.on("bye", () => {
+    addMessage("someone left ㅠㅠ");
+})
+
+socket.on("new_message", addMessage);
