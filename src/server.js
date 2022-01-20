bff29f8000a006fc601d054fa366ebce99c8d30a
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-20 10:42:08 +0900

    send object with callback function from client

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import http from "http";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import path from "path";
-import WebSocket, { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from "ws";
 const __dirname = path.resolve();
 
 const app = express();
@@ -16,9 +16,14 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
-wsServer.on("connection", socket => {
-    console.log(socket);
-})
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 3000);
+  });
+});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
