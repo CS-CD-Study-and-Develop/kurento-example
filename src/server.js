441e99c5393fd7c5453c5dfef6907476cc64fd3d
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-20 10:17:07 +0900

    connection with socketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import {Server} from "socket.io";
 import path from "path";
 import WebSocket, { WebSocketServer } from 'ws';
 const __dirname = path.resolve();
@@ -12,39 +13,12 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", socket => {
+    console.log(socket);
+})
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-// app.listen(3000, handleListen);
-
-const server = http.createServer(app);
-
-const wss = new WebSocketServer({ server });
-
-function handleConnection(socket) {
-    // console.log(socket);
-    console.log("connected");
-}
-
-const changeCharset = (message) => {
-    return Buffer.from(message, "base64").toString("utf-8");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anno";
-    console.log("connected to browser");
-    socket.on("close", () => console.log("disconnected from browser"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
