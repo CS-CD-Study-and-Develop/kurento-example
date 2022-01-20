bfd3367c527b382756319e88bb8125d0f56f9347
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-20 15:35:52 +0900

    counting user in rooms

--- src/server.js
+++ src/server.js
@@ -17,22 +17,24 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 function publicRooms() {
-    const {
-        sockets: {
-            adapter: {
-                sids, rooms
-            }
-        },
-    } = wsServer;
-    // const sids = wsServer.sockets.adapter.sids;
-    // const rooms = wsServer.sockets.adapter.rooms;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    })
-    return publicRooms;
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  // const sids = wsServer.sockets.adapter.sids;
+  // const rooms = wsServer.sockets.adapter.rooms;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 }
 
 wsServer.on("connection", (socket) => {
@@ -43,22 +45,22 @@ wsServer.on("connection", (socket) => {
   });
   socket.on("enter_room", (roomName, nickname, done) => {
     console.log(socket.id);
-    console.log(socket.rooms);
+    // console.log(socket.rooms);
     socket.join(roomName);
-    console.log(socket.rooms);
+    // console.log(socket.rooms);
     socket["nickname"] = nickname;
     done();
-    socket.to(roomName).emit("welcome", nickname);
+    socket.to(roomName).emit("welcome", nickname, countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname)
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
     );
   });
   socket.on("disconnect", () => {
     wsServer.sockets.emit("room_change", publicRooms());
-  })
+  });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
     done();
