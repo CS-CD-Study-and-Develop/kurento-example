a4624108fd3249e2de0cbd18fe2dc635f2f5231c
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-21 10:17:29 +0900

    add mute buttons

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,6 +5,8 @@ const muteBtn = document.querySelector("#mute")
 const camearBtn = document.querySelector("#camera")
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
 async function getMedia() {
   try {
@@ -20,3 +22,25 @@ async function getMedia() {
 }
 
 getMedia();
+
+function handleMuteClick() {
+  if(!muted) {
+    muteBtn.innerHTML = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerHTML = "Mute";
+    muted = false;
+  }
+}
+function handleCamearClick() {
+  if(cameraOff) {
+    camearBtn.innerHTML = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    camearBtn.innerHTML = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+camearBtn.addEventListener("click", handleCamearClick);
