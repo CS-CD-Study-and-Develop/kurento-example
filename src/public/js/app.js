fa48339e53e78de18ff3a4914045ea84698dfc3c
Noah <23338687+enjay27@users.noreply.github.com>
2022-01-21 14:25:00 +0900

    send and receive the offer via server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,6 +12,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName = "";
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -91,10 +92,11 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.querySelector("#welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -107,7 +109,20 @@ function handleWelcomeSubmit(event) {
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+});
 
-socket.on("welcome", () => {
-  console.log("someone joined");
-})
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
